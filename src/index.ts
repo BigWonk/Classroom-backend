2080349e4e72b9ae9a612ee0b3db39fc984d5699
Viktor D Stefanov <viktor.stefanov76@gmail.com>
2026-03-12 00:35:44 +0200

    initial-commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.get("/", (req, res)=>
+{
+res.send("Mazna brat");
+
+})
+
+
+
+
+
+
+
+
+app.listen(port, () =>
+{
+    console.log(`Server started at http://localhost:${port}`);
+})
