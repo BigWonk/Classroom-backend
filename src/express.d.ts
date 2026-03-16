015d945f91c2fc2ceae08a145487749b21b8fb8c
Viktor D Stefanov <viktor.stefanov76@gmail.com>
2026-03-17 00:07:14 +0200

    implement security

---
+++ src/express.d.ts
@@ -0,0 +1,14 @@
+declare global
+{
+    namespace Express
+    {
+        interface Request
+        {
+           user?:
+           {
+            role?: "admin" | "teacher" | "student";
+           }; 
+        }
+    }
+}
+export{};
